8c0a8c337f8bb4231fd1e0d644a2057fd92c0abb
Nirad <yeola.nirad@gmail.com>
2022-07-05 21:54:30 +0530

    Upload a new post feature added!

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,4 +1,13 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:instagram/models/user.dart';
+import 'package:instagram/providers/user_provider.dart';
+import 'package:instagram/resources/firestore_methods.dart';
+import 'package:instagram/utils/colors.dart';
+import 'package:instagram/utils/utils.dart';
+import 'package:provider/provider.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({Key? key}) : super(key: key);
@@ -8,10 +17,157 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+  bool _isLoading=false;
+  final TextEditingController _descriptionController = TextEditingController();
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text("Create a post"),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose a photo'),
+                onPressed: () async {
+                  Navigator.of(context).pop();
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Cancel'),
+                onPressed: ()  {
+                  Navigator.of(context).pop();
+                 
+                },
+              ),
+            ],
+          );
+        });
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
+  void clearImage() {
+    setState(() {
+      _file=null;
+    });
+  }
+  void postImage(
+    String uid,
+    String username,
+    String profImage,
+    ) async {
+      setState(() {
+        _isLoading=true;
+      });
+      try {
+        String res = await FirestoreMethods().uploadPost(_descriptionController.text, _file!, uid, username, profImage);
+        if (res=="Success") {
+          showSnackBar(context, "Posted");
+          clearImage();
+          setState(() {
+            _isLoading=false;
+          });
+        } else {
+          showSnackBar(context, res);
+        }
+      } catch(e) {
+        showSnackBar(context, e.toString());
+      }
+  }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Icon(Icons.add),
-    );
+    final User user = Provider.of<UserProvider>(context).getUser;
+    return _file == null
+        ? Center(
+            child: IconButton(
+                onPressed: () => _selectImage(context),
+                icon: Icon(Icons.upload)),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              title: Text('New Post'),
+              centerTitle: false,
+              leading: IconButton(
+                icon: Icon(Icons.close),
+                onPressed: clearImage,
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => postImage(
+                    user.uid,
+                    user.username,
+                    user.photoURL
+                  ),
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            body: Column(
+              children: [
+                _isLoading? const LinearProgressIndicator(): Padding(padding: EdgeInsets.only(top: 0),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      // backgroundImage: NetworkImage(user.photoURL),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      child: TextField(
+                        controller: _descriptionController,
+                        decoration: const InputDecoration(
+                          hintText: 'Write  a Caption',
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 8,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(image: MemoryImage(_file!)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
   }
 }
